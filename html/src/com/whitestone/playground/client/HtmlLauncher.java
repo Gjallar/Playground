af7bf6960aa1a2c6f7866d6643417f8b35a43a36
Flo <gjallar1@gmail.com>
2014-07-05 18:09:48 +0200

    Initial commit

---
+++ html/src/com/whitestone/playground/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.whitestone.playground.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.whitestone.playground.Gamemain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Gamemain();
+        }
+}
