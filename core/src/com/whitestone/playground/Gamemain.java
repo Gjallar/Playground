e17445477d715d86e0424025f307b62ed8045b56
Flo <gjallar1@gmail.com>
2014-07-05 18:16:55 +0200

    Second commit test

--- core/src/com/whitestone/playground/Gamemain.java
+++ core/src/com/whitestone/playground/Gamemain.java
@@ -14,6 +14,7 @@ public class Gamemain extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        System.out.println("Commit test!");
 	}
 
 	@Override
